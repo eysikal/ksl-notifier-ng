33be224304ffeb3f7250a028e15d03f445e9a9f4
Benjamin Russell <benjamin.russell@lendio.com>
2021-10-16 11:07:23 -0600

    Install Angular Material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { XSRFInterceptor } from './http-interceptors/xsrf-interceptor';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -13,7 +14,8 @@ import { XSRFInterceptor } from './http-interceptors/xsrf-interceptor';
   imports: [
     BrowserModule,
     HttpClientModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [
     { provide: HTTP_INTERCEPTORS, useClass: XSRFInterceptor, multi: true }
