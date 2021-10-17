8e2baa200f4e6d20e03c490983c18b6a05da98b6
Benjamin Russell <benjamin.russell@lendio.com>
2021-10-16 19:58:31 -0600

    Update proxy config to skip GET to /login

--- src/proxy.conf.js
+++ src/proxy.conf.js
@@ -1,15 +1,24 @@
-const PROXY_CONFIG = [
-  {
-      context: [
-          '/sanctum/csrf-cookie',
-          '/login',
-          '/logout',
-          '/api'
-      ],
-      target: 'http://localhost:8000',
+const apiURL = 'http://localhost:8000';
+const defaultConfig = {
+  target: apiURL,
+  secure: false,
+  logLevel: 'debug'
+};
+
+const PROXY_CONFIG ={
+  '/sanctum/csrf-cookie': defaultConfig,
+  '/logout': defaultConfig,
+  '/api': defaultConfig,
+  '/login': {
+      target: apiURL,
       secure: false,
-      logLevel: 'debug'
+      logLevel: 'debug',
+      bypass: (request) => {
+        if (request.headers.accept.indexOf('html') !== -1) {
+            return '/login';
+        }
+      }
   }
-];
+};
 
 module.exports = PROXY_CONFIG;
